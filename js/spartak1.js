94c87be7c02e0b651087c8f49d45c93dc6484f12
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2016-12-19 19:36:20 +0300

    cleanup js

--- js/spartak1.js
+++ js/spartak1.js
@@ -2,22 +2,14 @@ if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navig
 
 	var gallery = document.querySelector('.gallery');
 	var galleryImages = [
-		'./static/images/1/gallery/01.png',
-		'./static/images/1/gallery/02.png',
-		'./static/images/1/gallery/03.png',
-		'./static/images/1/gallery/04.png',
-		'./static/images/1/gallery/05.png',
-		'./static/images/1/gallery/06.png',
-		'./static/images/1/gallery/07.png',
-		'./static/images/1/gallery/08.png',
-		'./static/images/1/gallery/09.png',
-		'./static/images/1/gallery/10.png',
-		'./static/images/1/gallery/11.png',
-		'./static/images/1/gallery/12.png',
-		'./static/images/1/gallery/13.png',
-		'./static/images/1/gallery/14.png',
-		'./static/images/1/gallery/15.png',
-		'./static/images/1/gallery/16.png',
+		'./static/images/1/gallery/01.jpg',
+		'./static/images/1/gallery/02.jpg',
+		'./static/images/1/gallery/03.jpg',
+		'./static/images/1/gallery/04.jpg',
+		'./static/images/1/gallery/05.jpg',
+		'./static/images/1/gallery/06.jpg',
+		'./static/images/1/gallery/07.jpg',
+		'./static/images/1/gallery/08.jpg',
 	];
 	var activeImageIndex = 0;
 

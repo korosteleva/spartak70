d9a24f4558e3ec371ff34a1ce1fa211556b7296f
Tatyana Korosteleva <tvkorosteleva@avito.ru>
2016-12-20 16:37:06 +0300

    fix highlights

--- js/highlight.js
+++ js/highlight.js
@@ -1,39 +1,16 @@
 var highlights = document.querySelectorAll('.highlight');
 var animateClass = 'animate';
 var noAnimateClass = 'no-animate';
-window.addEventListener('scroll', throttle(function () {
+window.addEventListener('scroll', function () {
     [].forEach.call(highlights, (function(element) {
         if (isScrolledIntoView(element) && !element.classList.contains(animateClass) && !element.classList.contains(noAnimateClass)) {
             element.classList.add(animateClass);
         }
     }).bind(this));
-}, 100));
+});
 
 function isScrolledIntoView(el) {
     var elemTop = el.getBoundingClientRect().top;
     var elemBottom = el.getBoundingClientRect().bottom;
     return (elemTop >= 0) && (elemBottom <= window.innerHeight);
-}
-
-function throttle(fn, wait) {
-    var timeout, args, immediate, context;
-
-    return function() {
-        args = arguments;
-        immediate = true;
-        context = this;
-
-        if (!timeout) {
-            (function callback() {
-                if (immediate) {
-                    fn.apply(context, args);
-
-                    immediate = false;
-                    timeout = setTimeout(callback, wait);
-                } else {
-                    timeout = null;
-                }
-            })();
-        }
-    };
 }
